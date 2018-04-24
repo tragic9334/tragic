3e023b012d68c70834ab2a87169e17665e8ffd39
tragic9334 <38672252+tragic9334@users.noreply.github.com>
2018-04-24 17:34:56 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,6 +38,12 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'xoxo') {
+    	message.channel.send('Cute xo');
+  	}
+});
+
 client.on("message", async message => {
   // This event will run on every single message received, from any channel or DM.
   
@@ -74,6 +80,16 @@ client.on("message", async message => {
     // And we get the bot to say the thing: 
     message.channel.send(sayMessage);
   }
+  
+  //if(command === "say") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+   // const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+   // message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+   // message.channel.send(sayMessage);
+ // }
   
   if(command === "kick") {
     // This command must be limited to mods and admins. In this example we just hardcode the role names.
